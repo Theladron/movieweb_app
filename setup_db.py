872188a8edfa2efb72fbce13b8337caeaf70e1ab
Theladron <Philwolframm@gmail.com>
2025-04-21 18:10:02 +0200

    Added Setup file to create a new database with the necessary tables

---
+++ setup_db.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from datamanager.data_models import db
+
+# creating absolute path to the data folder
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+os.makedirs(DATA_DIR, exist_ok=True)
+
+# creating absolute path to the database
+DB_PATH = os.path.join(DATA_DIR, 'movies.db')
+
+# setting up flask and SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# creating the database with tables
+with app.app_context():
+    db.create_all()
+    print("Database and tables created successfully!")
